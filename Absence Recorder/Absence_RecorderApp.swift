0d4fc6d999c34a0387b56f5ec3c83041280cd27b
Leo Wildman <wildman.l@etoncollege.org.uk>
2023-01-28 18:48:47 +0000

    Initial Commit

---
+++ Absence Recorder/Absence_RecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Absence_RecorderApp.swift
+//  Absence Recorder
+//
+//  Created by Wildman, Leo (RCH) on 28/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Absence_RecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
